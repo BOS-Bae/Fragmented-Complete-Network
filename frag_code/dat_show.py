d504ed8bfee190d6e6b9894e3820e2e3b3810e28
BOS-Bae <ice970712@gmail.com>
2024-06-25 16:38:49 +0900

    make it more useful

--- frag_code/dat_show.py
+++ frag_code/dat_show.py
@@ -1,14 +1,15 @@
 import numpy as np
 import sys
 
-if (len(sys.argv) < 3):
-	print("python3 dat_show.py N rule_num error_idx confi_idx ")
+if (len(sys.argv) < 4):
+	print("python3 dat_show.py N idx_len rule_num confi_idx")
 	exit(1)
 
-N=int(sys.argv[1])
-r=int(sys.argv[2])
-e_idx=int(sys.argv[3])
-c_idx=int(sys.argv[4])
+N = int(sys.argv[1])
+idx_len = int(sys.argv[2])
+r = int(sys.argv[3])
+c_idx = int(sys.argv[4])
 
-data = np.loadtxt("./result_frag_N{}/L{}/idx{}.dat".format(N,r,e_idx))
-print(data[c_idx])
+for idx in range(idx_len):
+	data = np.loadtxt("./result_frag_N{}/L{}/idx{}.dat".format(N,r,idx))
+	print(idx, " ", data[c_idx])
