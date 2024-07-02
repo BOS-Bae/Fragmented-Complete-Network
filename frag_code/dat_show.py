da9df42dfa59f703b04b49a4dc4e22ec325aa770
BOS-Bae <ice970712@gmail.com>
2024-07-03 08:24:43 +0900

    dat show code edited

--- frag_code/dat_show.py
+++ frag_code/dat_show.py
@@ -11,5 +11,6 @@ r = int(sys.argv[3])
 c_idx = int(sys.argv[4])
 
 for idx in range(idx_len):
-	data = np.loadtxt("./result_frag_N{}/L{}/idx{}.dat".format(N,r,idx))
-	print(idx, " ", data[c_idx])
+	data = np.loadtxt("./result_frag_N{}/L{}/idx{}-avg.dat".format(N,r,idx))
+	dat = np.transpose(data)
+	print(idx, " ", dat[0,c_idx], " ", dat[1,c_idx])
