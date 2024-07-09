6b0e2e9afc51d3d2d393c05146bf69e14d6e3c07
BOS-Bae <ice970712@gmail.com>
2024-07-09 14:45:35 +0900

    stderr edited

--- frag_code/avg.py
+++ frag_code/avg.py
@@ -17,17 +17,16 @@ for i in range(1,M+1):
 confi_len = (len(list(np.loadtxt("./result_frag_N{}/L{}/num_data/idx0-sample0.dat".format(N, r_num)))) - 1)
 
 for idx in range(L):
-	tot_dat = np.zeros([n_s, confi_len])
-	for s in range(n_s):
-		dat = np.loadtxt("./result_frag_N{}/L{}/num_data/idx{}-sample{}.dat".format(N, r_num, idx, s))
-		tot_dat[s] = dat[0:confi_len]
-	std_err = np.std(tot_dat, 0)
-	avg = np.average(tot_dat, 0)
-	std_err /= n_s
-	std_err = np.sqrt(std_err)
-	dat_f = np.zeros([2, confi_len])
-	dat_f[0] = avg
-	dat_f[1] = std_err
-	dat_result = np.transpose(dat_f)
-	
-	np.savetxt("./result_frag_N{}/L{}/idx{}-avg.dat".format(N, r_num, idx), dat_result, fmt="%.6f")
+    tot_dat = np.zeros([n_s, confi_len])
+    for s in range(n_s):
+        dat = np.loadtxt("./result_frag_N{}/L{}/num_data/idx{}-sample{}.dat".format(N, r_num, idx, s))
+        tot_dat[s] = dat[0:confi_len]
+    std_err = np.std(tot_dat, 0)
+    std_err /= np.sqrt(n_s)
+    avg = np.average(tot_dat, 0)
+    dat_f = np.zeros([2, confi_len])
+    dat_f[0] = avg
+    dat_f[1] = std_err
+    dat_result = np.transpose(dat_f)
+
+    np.savetxt("./result_frag_N{}/L{}/idx{}-avg.dat".format(N, r_num, idx), dat_result, fmt="%.6f")
