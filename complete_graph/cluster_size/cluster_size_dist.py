average process corrected

complete_graph/cluster_size/cluster_size_dist.py
@@ -52,7 +52,7 @@ def size_distribution(c_size, M_max):
 def power_curve(m, a,b):
     return a*(np.power(m,b))
 
-c_dist_tot = np.zeros([n_s,M_max])
+c_dist_tot = []
 
 n_samples = 0
 for n in range(n_s):
@@ -64,9 +64,9 @@ for n in range(n_s):
         cluster_info = seek_cluster(N, image)
         c_size = cluster_size_info(cluster_info)
         c_dist_n = size_distribution(c_size, M_max)
-        c_dist_tot[n] = c_dist_n
+        c_dist_tot.append(c_dist_n)
 
-c_dist = np.average(c_dist_tot,0)
+c_dist = np.average(np.array(c_dist_tot),0)
 cumul_dist = np.zeros(len(c_dist))
 for i in range(len(c_dist)):
     cumul_dist[i] = sum(list(c_dist[i:]))
@@ -93,8 +93,8 @@ plt.title("N={}, L{}, {} MC samples".format(N, rule_num, n_samples))
 #plt.xticks(x_arr)
 plt.xlabel('M', fontsize=15)
 plt.ylabel('frequency', fontsize=15)
-#plt.show()
-plt.savefig("L{}_cluster_dist.png".format(rule_num))
+plt.show()
+#plt.savefig("L{}_cluster_dist.png".format(rule_num))
 plt.clf()
 
 plt.plot(x_arr, cumul_dist, label="numerical data, F(m <= X)", marker = 'o', color='blue')
@@ -109,8 +109,8 @@ plt.title("N={}, L{}, {} MC samples".format(N, rule_num, n_samples))
 #plt.xticks(x_arr)
 plt.xlabel('M', fontsize=15)
 plt.ylabel('cumulative frequency', fontsize=15)
-#plt.show()
-plt.savefig("L{}_cluster_cumul_dist.png".format(rule_num))
+plt.show()
+#plt.savefig("L{}_cluster_cumul_dist.png".format(rule_num))
 
 '''
 plus=0

complete_graph/cluster_size/opinion_avg.py
@@ -14,19 +14,19 @@ n_s = int(sys.argv[3])
 nbins = int(sys.argv[4])
 M_max = N
 
-op_dist = np.zeros(n_s)
+op_dist = []
+n_samples = 0
 for n in range(n_s):
     file = "./N{}_L{}_dat/N{}_L{}_image_s{}".format(N, rule_num, N, rule_num, n)
-    if os.path.getsize(file) > 0: 
+    if os.path.getsize(file) > 0:
         image = np.loadtxt(file)
         n_samples += 1
-        #image = np.loadtxt("./N{}_L{}_dat/N{}_L{}_image_s{}".format(N, rule_num, N, rule_num, n))
-				op_dist[n] = np.average(image)
+        op_dist.append(np.average(image))
 
 plt.hist(op_dist, bins=nbins, color="black", range = (-1,1))
 plt.xticks(np.arange(-1,1.5,0.5),fontsize=13)
 plt.yticks(fontsize=13)
-plt.title("N={}, L{}, {} MC samples".format(N, rule_num, n_s))
+plt.title("N={}, L{}, {} MC samples".format(N, rule_num, n_samples))
 #plt.xticks(x_arr)
 plt.xlabel('average opinion', fontsize=15)
 plt.show()