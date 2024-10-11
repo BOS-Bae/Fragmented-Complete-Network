87bc9fbdc52c5d11a9b3bc4bd6171b01e674b238
BOS-Bae <ice970712@gmail.com>
2024-10-11 17:09:35 +0900

    revised in advance

--- complete_graph/cluster_size/parameter2D_search.py
+++ complete_graph/cluster_size/parameter2D_search.py
@@ -4,24 +4,24 @@ from scipy.optimize import curve_fit
 from matplotlib.ticker import FixedLocator, FixedFormatter
 import sys
 
-if (len(sys.argv) < 3):
-	print("python3 parameter2D_search.py c2_sl M_sl xmin")
+if (len(sys.argv) < 6):
+	print("python3 parameter2D_search.py c2 c1_start c1_fin M_start M_fin xmin")
 	exit(1)
 
-c1 = 1
-c2_sl = int(sys.argv[1])
-M_sl = int(sys.argv[2])
-xmin = int(sys.argv[3])
+c2 = float(sys.argv[1])
+c1_start = float(sys.argv[2])
+c1_fin = float(sys.argv[3])
+M_start = float(sys.argv[4])
+M_fin = float(sys.argv[5])
+xmin = int(sys.argv[6])
 
+real_exponent = -3.4
 N = 500
-c2_start = 0.01
-c2_fin = 0.9
-M_start = 0.1
-M_fin = 3.0
+c1_sl = M_sl = 50
 
-c2_arr = np.linspace(c2_start, c2_fin, c2_sl)
+c1_arr = np.linspace(c1_start, c1_fin, c1_sl)
 M_arr = np.linspace(M_start, M_fin, M_sl)
-print(c2_arr)
+print(c1_arr)
 print(M_arr)
    # Initial condition c[0]
 A = 0.64
@@ -33,48 +33,65 @@ def frac(k):
 def Q(k):
 	A = 0.64
 	B = 0.84
-	return A*(k ** B)
+	return A*(k ** (1-B))
 
 
 def power_law(k, a, b):
 	return a * k ** b
 
 
-def solve_fit_diff_eq(c1, c2_arr, M_arr, N):
-	result_mat = np.zeros([len(list(c2_arr)), len(list(M_arr))])
+def solve_fit_diff_eq(c2, c1_arr, M_arr, N):
+	result_mat = np.zeros([len(list(M_arr)), len(list(c1_arr))])
 	k_values = np.arange(xmin, N+1)
 	
 	for M_idx in range(len(list(M_arr))):
 		M = M_arr[M_idx]
-		for c2_idx in range(len(list(c2_arr))):
-			c2 = c2_arr[c2_idx]
+		for c1_idx in range(len(list(c1_arr))):
+			c1 = c1_arr[c1_idx]
 			c = np.zeros(N)
 			c[0] = c1
 			c[1] = c2
 	
-			for k in range(3, N+1):
+			for k in range(2, N):
 				k_idx = k - 1
-				c[k_idx] = frac(k-1)*c[k_idx-1] - (M*c1/(k-1))*(c[k_idx-2]*Q(k-2) - c[k_idx-1]*Q(k-1))
+				c[k_idx+1] = frac(k)*c[k_idx] - (M**2)*N*(c1/k)*(c[k_idx-1]*Q(k-1) - c[k_idx]*Q(k))
+			#for k in range(3, N+1):
+			#	k_idx = k - 1
+			#	c[k_idx+1] = frac(k)*c[k_idx] - (M**2)*N*(c1/k)*(c[k_idx-1]*Q(k-1) - c[k_idx]*Q(k))
 		
+			summ = 0
+			check = 0
+			for k in range(1, N+1):
+				k_idx = k-1
+				if (c[k_idx] < 0) : check += 1
+				summ += k*c[k_idx]
+			c /= summ; c[0] = c1; c[1] = c2
+
 			c_values = c[xmin-1:N]
 			popt, _ = curve_fit(power_law, k_values, c_values)
-	
-			result_mat[M_idx, c2_idx] = popt[1]
+			
+			if (check == 0) :	result_mat[M_idx, c1_idx] = popt[1]
+			if (check != 0) :	result_mat[M_idx, c1_idx] = 100
+		
 	
 	return result_mat
 
-def plot_fig(c2_arr, M_arr, Mat):
+def plot_fig(c1_arr, M_arr, Mat):
 	fig, ax = plt.subplots()
-	cax = ax.imshow(Mat, cmap='viridis', origin='upper')  # Use `origin='upper'` to match matrix indexing
+	Mat_raw = Mat
+	Mat = abs(Mat - real_exponent)
+	for i in range(len(M_arr)):
+		for j in range(len(c1_arr)):
+			if (Mat_raw[i,j] == 100): Mat_raw[i,j] = 0
+	cax = ax.imshow(Mat_raw, cmap='terrain', origin='upper')  # Use `origin='upper'` to match matrix indexing
+
 	min_idx = np.unravel_index(np.argmin(Mat), Mat.shape)
 	
-	# Get the x and y indices
-	c2_idx, M_idx = min_idx
+	M_idx, c1_idx = min_idx
 	
-	# Get the x and y values from the original arrays
-	c2_val = c2_arr[c2_idx]  # x-values correspond to columns (second index)
+	c1_val = c1_arr[c1_idx]  # x-values correspond to columns (second index)
 	M_val = M_arr[M_idx]  # y-values correspond to rows (first index)
-	xticks = np.linspace(0,len(c2_arr)-1, 5)
+	xticks = np.linspace(0,len(c1_arr)-1, 5)
 	yticks = np.linspace(0,len(M_arr)-1, 5)
 	ax.set_xticks(xticks)
 	ax.set_yticks(yticks)
@@ -82,16 +99,16 @@ def plot_fig(c2_arr, M_arr, Mat):
 
 	x_show = [];	y_show = []
 	for x in xticks:
-		x_show.append(np.round(c2_arr[int(x)],2))
+		x_show.append(np.round(c1_arr[int(x)],2))
 	for y in yticks:
 		y_show.append(np.round(M_arr[int(y)],2))
-
+	
 	ax.set_xticklabels(x_show, fontsize=12)
 	ax.set_yticklabels(y_show, fontsize=12)
-	plt.xlabel('c2', fontsize=15)
+	plt.xlabel('c1', fontsize=15)
 	plt.ylabel('M', fontsize=15)
-	plt.title("c2={:.2f}, M={:.2f}, best alpha={:.2f}".format(c2_val, M_val, np.min(Mat)))
+	plt.title("c2={} / c1={:.2f}, M={:.2f}, best alpha={:.2f}".format(c2, c1_val, M_val, Mat_raw[M_idx, c1_idx]))
 	plt.show()
 
-Mat = solve_fit_diff_eq(c1, c2_arr, M_arr, N)
-plot_fig(c2_arr, M_arr, Mat)
+Mat = solve_fit_diff_eq(c2, c1_arr, M_arr, N)
+plot_fig(c1_arr, M_arr, Mat)
--- complete_graph/cluster_size/plot_diff_sol.py
+++ complete_graph/cluster_size/plot_diff_sol.py
@@ -3,15 +3,19 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import sys
 
-c1 = float(sys.argv[1])
-c2 = float(sys.argv[2])
-xmin = int(sys.argv[3])
+if (len(sys.argv) < 4):
+	print("python3 plot_diff_sol.py c1 c2 M xmin")
+	exit(1)
+
+c1 = float(sys.argv[1]) 
+c2 = float(sys.argv[2]) 
+M = float(sys.argv[3])
+xmin = int(sys.argv[4])
 
 N = 500
 M_start = 0.01
 M_fin = 1.00
 #M = 0.38
-M = 2.38
 
 A = 0.64
 B = 0.84
@@ -24,11 +28,11 @@ def frac(k):
 def Q(k):
 	A = 0.64
 	B = 0.84
-	return A*(k ** B)
+	return A * (k ** (1-B))
 
 
 def power_law(k, a, b):
-	return a * k ** b
+	return a * (k ** b)
 
 
 def solve_difference_equation(c1, c2, M, N):
@@ -36,10 +40,20 @@ def solve_difference_equation(c1, c2, M, N):
 	c[0] = c1
 	c[1] = c2
 
-	for k in range(3, N+1):
+	for k in range(2, N):
 		k_idx = k - 1
-		c[k_idx] = frac(k-1)*c[k_idx-1] - (M*c1/(k-1))*(c[k_idx-2]*Q(k-2) - c[k_idx-1]*Q(k-1))
+		c[k_idx+1] = frac(k)*c[k_idx] - (M**2)*N*(c1/k)*(c[k_idx-1]*Q(k-1) - c[k_idx]*Q(k))
 	
+	summ = 0
+	M_num = 0
+	print(c)
+	for k in range(1,N+1):
+		k_idx = k - 1
+		M_num += c[k_idx]
+		summ += k*c[k_idx]
+	c[2:] /= summ
+	#c[0] = c1; c[1] = c2
+	print(M_num)
 	return c
 
 
@@ -54,16 +68,18 @@ def fit_power(c, N, xmin):
 def plot_fig(c_values, c_parameters):
 	k_values = np.arange(1, N+1)
 	plt.scatter(k_values, c_values, label='numerical solution', color='blue')
-	plt.plot(k_values, power_law(k_values, c_parameters[0], c_parameters[1]), label='fitted curve: {:.2f}*k^{:.2f}'.format(c_parameters[0], c_parameters[1]), color='red')
+	plt.plot(k_values, power_law(k_values, c_parameters[0], c_parameters[1]), label='exponent : {:.2f}'.format(c_parameters[1]), color='red')
 	
-	plt.xlabel('k')
-	plt.ylabel('c[k]')
+	plt.xlabel('k',fontsize=18)
+	plt.ylabel('c[k]',fontsize=18)
+	plt.xticks(fontsize=13)
+	plt.yticks(fontsize=13)
 	plt.legend(fontsize=15)
 	plt.xscale('log')
 	plt.yscale('log')
+	plt.title('c1={}, c2={}, M={}, xmin={}'.format(c1,c2,M,xmin), fontsize=19)
 	plt.show()
 	
-
 c_values = solve_difference_equation(c1, c2, M, N)
 fit_parameters = fit_power(c_values, N, xmin)
 fit_exponent = fit_parameters[1]
