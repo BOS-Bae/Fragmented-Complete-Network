474af9b8f0dbc27d22278441728b5c95b3fc86fa
BOS-Bae <ice970712@gmail.com>
2024-11-17 08:09:08 +0900

    fig generated

--- complete_graph/L8_confi_dat/L8/Confi_save.sh
+++ complete_graph/L8_confi_dat/L8/Confi_save.sh
@@ -5,4 +5,3 @@ cidx=$2
 ./number_show.sh $N $cidx > ./flip_dat/uni-N$N-L8-idx$cidx.dat
 python3 prob_cal.py $N $cidx > ./flip_dat/prob-N$N-L8-idx$cidx.dat
 ./mat_show $N $cidx > confi-N$N-idx$cidx.dat
-python3 draw_sol_graph.py $N $cidx
--- complete_graph/L8_confi_dat/L8/draw_sol_graph.py
+++ complete_graph/L8_confi_dat/L8/draw_sol_graph.py
@@ -22,36 +22,42 @@ def print_coupled_equations(w_connections, M):
 
 
 if (len(sys.argv) < 2):
-    print("python3 draw_sol_graphy.py N idx")
+    print("python3 draw_sol_graphy.py N idx spectral_or_spring")
     exit(1)
 
 N = int(sys.argv[1])
 idx = int(sys.argv[2])
+spectral_or_spring = int(sys.argv[3])
+
 M = N*N
 
 file_path = "./flip_dat/prob-N{}-L8-idx{}.dat".format(N, idx)
 weight_connections = np.loadtxt(file_path, dtype=int)
-connections = np.loadtxt(file_path, dtype=int, usecols=(0, 1))
 print_coupled_equations(weight_connections, M)
 
 G = nx.DiGraph()
 
-G.add_edges_from(connections)
+for source, target, weight in weight_connections:
+	G.add_edge(source, target, weight=weight)
+
+#edge_labels = { (source, target): f"{weight}/{M}" for source, target, weight in weight_connections}
 
 self_edges = list(nx.selfloop_edges(G))
 G.remove_edges_from(self_edges)
 
-nodes_with_no_outgoing_links = [node for node,
-                                out_degree in G.out_degree() if out_degree == 0]
+if (spectral_or_spring == 0):
+	pos = nx.spectral_layout(G)
+	
+else : pos = nx.spring_layout(G)
 
-node_colors = [
-    'red' if node in nodes_with_no_outgoing_links else 'lightblue' for node in G.nodes]
+nodes_with_no_outgoing_links = [node for node, out_degree in G.out_degree() if out_degree == 0]
 
-# pos = nx.spring_layout(G)
-pos = nx.spectral_layout(G)
-
+node_colors = ['red' if node in nodes_with_no_outgoing_links else 'lightblue' for node in G.nodes]
+print(nodes_with_no_outgoing_links)
 plt.figure(figsize=(8, 6))
-nx.draw(G, pos, with_labels=True, node_size=700, node_color=node_colors,
-        font_size=10, font_weight="bold", arrows=True)
+nx.draw(G, pos, with_labels=True, node_size=200, node_color=node_colors,
+        font_size=8, font_weight="bold", arrows=True, arrowsize=10)
+
+#nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
 
 plt.show()
