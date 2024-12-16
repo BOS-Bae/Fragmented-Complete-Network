0d243b6241dd23e30f4c0707edcfd59e82680abb
BOS-Bae <ice970712@gmail.com>
2024-12-16 17:03:04 +0900

    okay

--- complete_graph/L8_confi_dat/L8/P_R_save.py
+++ complete_graph/L8_confi_dat/L8/P_R_save.py
@@ -4,15 +4,22 @@ N = 100
 R_mat = np.zeros([N+1,N+1])
 P_mat = np.zeros([N+1,N+1])
 
+original_path = "./L8-N100-p-r-result.txt"
+n1_path = "./L8-N100-n1-p-r-result.txt"
+
 def convert_complex(s):
     return complex(s.decode('utf-8').replace(" I", "j").replace(" ", ""))
 
-file_path = "./L8-N{}-p-r-result.txt".format(N)
-with open(file_path, 'r') as f:
-		raw_data = f.readlines()
+save_path = "./L8-N{}-p-r-result-revised.txt".format(N)
 
-data = []
-for line in raw_data:
+with open(original_path, 'r') as f:
+		original_data = f.readlines()
+
+with open(n1_path, 'r') as f:
+		n1_data = f.readlines()
+
+data = []; data_n1 = []
+for line in original_data:
     parts = line.strip().split(',')
     row = [
         int(parts[0]),
@@ -21,16 +28,30 @@ for line in raw_data:
     ]
     data.append(row)
 
-print(data)
-#data = np.loadtxt(file_path, delimiter=",", converters={2: convert_complex})
+for line in n1_data:
+    parts = line.strip().split(',')
+    row = [
+        int(parts[0]),
+        int(parts[1]),
+        convert_complex(parts[2].encode('utf-8'))
+    ]
+    data_n1.append(row)
 
 for dat in data:
     m = dat[0]
     n = dat[1]
     R = dat[2].imag
-    P = 1-(dat[2].real + R)
+    P = 1.0000000000000000 -(dat[2].real + R)
     R_mat[int(m),int(n)] = R
     P_mat[int(m),int(n)] = P
 
+for dat_new in data_n1:
+	m = dat_new[0]
+	n = dat_new[1]
+	R = dat_new[2].imag
+	P = 1.0000000000000000 -(dat_new[2].real + R)
+	R_mat[int(m),int(n)] = R
+	P_mat[int(m),int(n)] = P
+print(data_n1)	
 np.savetxt("./L8-prob-R.dat",R_mat)
 np.savetxt("./L8-prob-P.dat",P_mat)
---
+++ complete_graph/L8_confi_dat/L8/compare_m_fixed.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+if (len(sys.argv) < 2):
+	print("python3 compare.py n log_setting(1)")
+	exit(1)
+
+m = int(sys.argv[1])
+log_or_not = int(sys.argv[2])
+
+P_exact = np.loadtxt("./L8-prob-P.dat")
+R_exact = np.loadtxt("./L8-prob-R.dat")
+P_num_dat = np.loadtxt("../../prob_dat/dat/p_L8")
+R_num_dat = np.loadtxt("../../prob_dat/dat/r_L8")
+
+n_list = range(14+1)
+P_exact_m1 = np.zeros(15)
+R_exact_m1 = np.zeros(15)
+P_num_m1 = np.zeros(15)
+R_num_m1 = np.zeros(15)
+P_err_m1 = np.zeros(15)
+R_err_m1 = np.zeros(15)
+
+for n in n_list:
+	P_exact_m1[n] = P_exact[m,n]
+	R_exact_m1[n] = R_exact[m,n]
+	
+for l_idx in range(len(P_num_dat)):
+	dat_p = P_num_dat[l_idx]
+	dat_r = R_num_dat[l_idx]
+	if (dat_p[1] <= 14 and int(dat_p[0]) == m and dat_p[0] != dat_p[1]):
+		P_num_m1[int(dat_p[1])] = dat_p[2]
+		P_err_m1[int(dat_p[1])] = dat_p[3]
+	if (dat_r[1] <= 14 and int(dat_r[0]) == m and dat_r[0] != dat_r[1]):
+		R_num_m1[int(dat_p[1])] = dat_r[2]
+		R_err_m1[int(dat_p[1])] = dat_r[3]
+
+plt.figure(figsize=(10,7))
+P_num_m1[P_num_m1 == 0] = np.nan
+plt.plot(n_list[2:], P_exact_m1[2:], color='blue', label = "derived", marker='o')
+plt.plot(n_list[2:], P_num_m1[2:], color='lime', label = "numerical", marker='o')
+plt.errorbar(n_list[2:], P_num_m1[2:], yerr=P_err_m1[2:], color='darkblue')
+plt.xlabel('m', fontsize=16)
+plt.ylabel('P({},n)'.format(m), fontsize=16)
+plt.xticks(fontsize=17)
+plt.yticks(fontsize=17)
+plt.legend(fontsize=16)
+if (log_or_not == 1): plt.yscale('log')
+plt.show()
+
+plt.figure(figsize=(10,7))
+R_num_m1[R_num_m1 == 0] = np.nan
+plt.plot(n_list[2:], R_exact_m1[2:], color='blue', label = "derived", marker='o')
+plt.plot(n_list[2:], R_num_m1[2:], color='lime', label = "numerical", marker='o')
+plt.errorbar(n_list[2:], R_num_m1[2:], yerr=R_err_m1[2:], color='darkblue')
+plt.xlabel('m', fontsize=16)
+plt.ylabel('R({},n)'.format(m), fontsize=16)
+plt.xticks(fontsize=17)
+plt.yticks(fontsize=17)
+plt.legend(fontsize=16)
+
+if (log_or_not == 1): plt.yscale('log')
+plt.show()
+
